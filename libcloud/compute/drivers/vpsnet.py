31cfcfa5fb1c1138659b6e058a5822124eec3eae
mfallone <m.fallone@psiphon.ca>
2016-03-10 11:30:52 -0500

    Added ability to manage SSD VPS instances.

--- libcloud/compute/drivers/vpsnet.py
+++ libcloud/compute/drivers/vpsnet.py
@@ -191,3 +191,131 @@ class VPSNetNodeDriver(NodeDriver):
 
     def list_locations(self):
         return [NodeLocation(0, "VPS.net Western US", 'US', self)]
+    
+    def list_ssd_nodes(self, location=None):
+        res = self.connection.request(
+            '/ssd_virtual_machines.%s' % (API_VERSION,))
+        
+        return [self._to_ssd_node(i['virtual_machine']) for i in res.object]
+    
+    def get_ssd_node(self, node_id):
+        res = self.connection.request(
+            '/ssd_virtual_machines/%s.%s' % (node_id,
+                                             API_VERSION),
+            method="GET")
+        node = self._to_ssd_node(res.object['virtual_machine'])
+        return node
+    
+    def reboot_ssd_node(self, node):
+        res = self.connection.request(
+            '/ssd_virtual_machines/%s/%s.%s' % (node.id,
+                                                'reboot',
+                                                API_VERSION),
+            method="POST")
+        node = self._to_ssd_node(res.object['virtual_machine'])
+        return True
+    
+    def shutdown_ssd_node(self, node):
+        res = self.connection.request(
+            '/ssd_virtual_machines/%s/%s.%s' % (node.id,
+                                                'shutdown',
+                                                API_VERSION),
+            method="POST")
+        node = self._to_ssd_node(res.object['virtual_machine'])
+        return True
+    
+    def force_shutdown_ssd_node(self, node):
+        res = self.connection.request(
+            '/ssd_virtual_machines/%s/%s.%s' % (node.id,
+                                                'power_off',
+                                                API_VERSION),
+            method="POST")
+        node = self._to_ssd_node(res.object['virtual_machine'])
+        return True
+    
+    def start_ssd_node(self, node):
+        res = self.connection.request(
+            '/ssd_virtual_machines/%s/%s.%s' % (node.id,
+                                                'power_on',
+                                                API_VERSION),
+            method="POST")
+        node = self._to_ssd_node(res.object['virtual_machine'])
+        return True
+    
+    def delete_ssd_node(self, node):
+        res = self.connection.request(
+            '/ssd_virtual_machines/%s.%s' % (node.id,
+                                             API_VERSION),
+            method="DELETE")
+        return res
+    
+    def create_ssd_node(self, fqdn, image_id, cloud_id, size, **kwargs):
+        """Create a new VPS.net node
+
+        @inherits: :class:`NodeDriver.create_node`
+
+        :keyword    ex_backups_enabled:       Enable automatic backups
+        :type       ex_backups_enabled:       ``bool``
+
+        :keyword    ex_rsync_backups_enabled: Fully Qualified domain of the node
+        :type       ex_rsync_backups_enabled:   ``bool``
+        """
+        headers = {'Content-Type': 'application/json'}
+        request = {'virtual_machine':
+                   {'ssd_vps_plan': size,
+                    'fqdn': fqdn,
+                    'system_template_id': image_id,
+                    'cloud_id': cloud_id,
+                    'backups_enabled': kwargs.get('backups_enabled', 0),
+                    'rsync_backups_enabled': kwargs.get(
+                        'rsync_backups_enabled', 0)}}
+        
+        res = self.connection.request(
+            '/ssd_virtual_machines.%s' % (API_VERSION,),
+            data=json.dumps(request),
+            headers=headers,
+            method='POST')
+        if 'virtual_machine' in res.object:
+            node = self._to_ssd_node(res.object['virtual_machine'])
+        else:
+            node = res
+        return node
+    
+    def _to_ssd_node(self, vm):
+        if vm['running']:
+            state = NodeState.RUNNING
+        else:
+            state = NodeState.PENDING
+
+        n = Node(id=vm['id'],
+                 name=vm['ssd_vps_label'],
+                 state=state,
+                 public_ips=[vm.get('primary_ip_address', None)],
+                 private_ips=[],
+                 extra={
+                    'password': vm.get('password', None),
+                    'backups_enabled': vm['backups_enabled'],
+                    'cloud_id': vm['cloud_id']},
+                 driver=self.connection.driver)
+        return n
+    
+    def get_system_templates(self):
+        res = self.connection.request(
+            '/system_templates.%s' % (API_VERSION),
+            method="GET")
+        return res
+    
+    def get_all_ssd_clouds(self):
+        res = self.connection.request(
+            '/available_ssd_clouds.%s' % (API_VERSION),
+            method='GET')
+        return res.parse_body()
+    
+    def get_available_ssd_clouds(self):
+        available_clouds = list()
+        clouds = self.get_all_ssd_clouds()
+        for cloud in clouds:
+            if cloud['cloud']['available']:
+                available_clouds.append(cloud)
+        
+        return available_clouds
